61c61be35e7f97da7c4130eab73212e567125c6d
AlexArnaboldi <alex.arnaboldi97@hotmail.com>
2025-09-26 18:45:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -37,3 +37,19 @@ window.addEventListener("scroll", () => {
     alterStyles(isBackToTopRendered);
   }
 });
+
+
+
+function openModal(id) {
+  document.getElementById(id).style.display = 'block';
+}
+
+function closeModal(id) {
+  document.getElementById(id).style.display = 'none';
+}
+
+window.addEventListener('click', function(e) {
+  if (e.target.classList.contains('modal')) {
+    e.target.style.display = 'none';
+  }
+});
